724816824544114b80ffdabdd8ab696ee2e7475b
Davi Oliveira <103294834+Raidercoll@users.noreply.github.com>
2022-08-31 13:52:53 -0300

    Update Vagao.java

--- TrabalhoTrem/TrabalhoTrem/src/Vagao.java
+++ TrabalhoTrem/TrabalhoTrem/src/Vagao.java
@@ -1,7 +1,32 @@
 public class Vagao {
+    private int id;
+    private double capacMax;
+    private Trem trem;
 
-    private int identificadorVagao;
-    private double capacidadeMax; //Capacidade máxima em toneladas
-    //Referencia ao trem que faz parte no momento ou null se está livre
-    
+    public Vagao(int id, int capacMax){
+        this.id = id;
+        this.capacMax = capacMax;
+        this.trem = null;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public double getCapacMax(){
+        return capacMax;
+    }
+
+    public Trem getTrem(){
+        return trem;
+    }
+
+    public void setTrem(Trem trem){
+        this.trem = trem;
+    }
+
+    @Override
+    public String toString() {
+        return "Vagao [capacMax=" + capacMax + ", id=" + id + ", trem=" + trem + "]";
+    }
 }
