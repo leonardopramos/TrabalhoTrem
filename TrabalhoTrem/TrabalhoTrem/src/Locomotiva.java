5d7fbe79787ad58cf60bdab6688a577f59003e73
Davi Oliveira <103294834+Raidercoll@users.noreply.github.com>
2022-08-31 12:57:04 -0300

    Atualizando a locomotiva
    Locomotiva ja com os getters e setters

--- TrabalhoTrem/TrabalhoTrem/src/Locomotiva.java
+++ TrabalhoTrem/TrabalhoTrem/src/Locomotiva.java
@@ -1,8 +1,39 @@
-public class Locomotiva {
-
-    private int identificadorLocomotiva;
-    private double pesoMax; //É o peso máximo que consegue puxar em toneladas
-    private int numMaxVagoes; //Número máximo de vagões que consegue tracionar
-    //Referencia para o trem que faz parte no momento ou null se esta livre;
+public class Locomotiva{
+    private int id;
+    private double pesoMax;
+    private int nroMaxVagoes;
+    private Trem trem;
     
+    public Locomotiva(int id, double pesoMax, int nroMaxVagoes) {
+        this.id = id;
+        this.pesoMax = pesoMax;
+        this.nroMaxVagoes = nroMaxVagoes;
+        this.trem = null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getPesoMax() {
+        return pesoMax;
+    }
+
+    public int getNroMaxVagoes() {
+        return nroMaxVagoes;
+    }
+
+    public Trem getTrem() {
+        return trem;
+    }
+
+    public void setTrem(Trem trem){
+        this.trem = trem;
+    }
+
+    @Override
+    public String toString() {
+        return "Locomotiva [id=" + id + ", nroMaxVagoes=" + nroMaxVagoes + ", pesoMax=" + pesoMax + ", trem=" + trem
+                + "]";
+    }
 }
